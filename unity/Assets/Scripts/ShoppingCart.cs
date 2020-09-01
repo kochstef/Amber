384b0a43b49833cd32fdf2269e90391f7b135f11
Stefan Kocher <stefan.kocher@student.tugraz.at>
2020-09-01 19:13:58 +0200

    Interior design and the functionaltiy for the shoping cart. Implementation of the viewing logic for the shoping list

--- unity/Assets/Scripts/ShoppingCart.cs
+++ unity/Assets/Scripts/ShoppingCart.cs
@@ -1,21 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShoppingCart : MonoBehaviour
 {
-    /*
-     public Transform player;
+    
+    public Transform player;
      
     public float distanceTillTeleport = 1f;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    public GameObject findClosestShelf()
+    private GameObject findClosestShelf()
     {
         GameObject[] gos;
         gos = GameObject.FindGameObjectsWithTag("PositionShoppingCart");
@@ -35,30 +31,12 @@ public class ShoppingCart : MonoBehaviour
         return closest;
     }
     
-    // Update is called once per frame
     private void teleportShopingCart()
     {
-        Debug.Log("TeleportShoppingCart");
+        //Debug.Log("TeleportShoppingCart");
         GameObject closestShelf = findClosestShelf();
         transform.position = closestShelf.transform.position;
         transform.rotation = closestShelf.transform.rotation;
-        /* Vector3 pos = player.InverseTransformPoint(closestShelf.transform.position);
- 
-         if (pos.z < 0) 
-             Debug.Log("Back ");
-         else 
-             Debug.Log("Front ");
- 
-         if (pos.x < 0)
-             Debug.Log("Left ");
-         else
-             Debug.Log("Right ");
-         */
-
-        /*  Transform shoppingCartPosition = transform.Find("PositionShoppingCart");
-          if(shoppingCartPosition != null){
-         = shoppingCartPosition.position;
-          }
     }
     void Update()
     {
@@ -66,5 +44,10 @@ public class ShoppingCart : MonoBehaviour
         {
             teleportShopingCart();
         }
-    }*/
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        other.transform.parent = transform;
+    }
 }
---
+++ unity/Assets/Scripts/ShoppingList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShoppingList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject testCube;
+    private bool isCounting;
+    public float timeToShowShoppingList = 3f;
+    private float countdown;
+    //public TextMeshPro textmeshPro;
+    void Start()
+    {
+        testCube.SetActive(false);
+        isCounting = false;
+        countdown = 3f; 
+       // textmeshPro = GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.left);
+        Debug.DrawLine(transform.position, fwd, Color.green);
+        RaycastHit hit;
+        
+        
+        if (Physics.Raycast(transform.position, fwd, out hit, 10) && hit.collider.CompareTag("Head"))
+        { 
+            isCounting = true;
+        }
+        else 
+        {
+            isCounting = false;
+            //This happens every frame the user isn't looking at an "option" so this could be optimized. But not a big problem.
+            countdown = timeToShowShoppingList;
+            //textmeshPro.renderer.enabled = false;
+            testCube.SetActive(false);
+        }
+ 
+        if(countdown <= 0f) {
+            //Select option
+           textmeshPro.renderer.enabled = true;
+           testCube.SetActive(true);
+           Debug.Log(countdown);
+           
+        }
+        if(isCounting) countdown -= Time.deltaTime;
+      
+    }
+}
