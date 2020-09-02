da12e894279c74b6f62a0070b6db38c8b2fb61c2
Stefan Kocher <stefan.kocher@student.tugraz.at>
2020-09-02 18:53:20 +0200

    Creation of the shoping list and functionality for the shoping cart (counting of the right and wrong items in the basket)

--- unity/Assets/Scripts/ShoppingList.cs
+++ unity/Assets/Scripts/ShoppingList.cs
@@ -5,27 +5,90 @@ using TMPro;
 
 public class ShoppingList : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject testCube;
+    
+    // For showing the list------------------------------
+    public GameObject listObject;
     private bool isCounting;
     public float timeToShowShoppingList = 3f;
     private float countdown;
-    //public TextMeshPro textmeshPro;
+ 
+    //For filling the List-------------------------------
+    public TextMeshPro textmeshPro;
+    private List<string> allItems;
+    //this later should go over the server depending on the level of the patient
+    private int amountOfItems = 6;
+    private Dictionary<string, int> itemsToCollect;
+    //the shoppingcart to check if all items have been collected
+    private GameObject shoppingCart;
+    
     void Start()
     {
-        testCube.SetActive(false);
+        listObject.SetActive(false);
         isCounting = false;
         countdown = 3f; 
-       // textmeshPro = GetComponent<TextMeshPro>();
+        
+        //------------------------------------------
+        allItems = new List<string>();
+        allItems.Add("brown cube");
+        allItems.Add("green cube");
+        itemsToCollect = new Dictionary<string, int>();
+        RandomItemsToCollect();
+        shoppingCart = GameObject.Find("Shoppingcart");
+        
+        SetText();
+        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    
+    //gets a random item from all items and puts it in a dictanary or increases the counter of the item
+     private void RandomItemsToCollect()
+    {
+        List<string> items_to_collect = new List<string>();
+
+        int randomIndex;
+        string item_name;
+        for (int i = 0; i < amountOfItems; i++)
+        {
+            
+            randomIndex = Random.Range(0, allItems.Count);
+            item_name = allItems[randomIndex];
+            if(itemsToCollect.ContainsKey(item_name))
+            {
+                itemsToCollect[item_name] = itemsToCollect[item_name] + 1;
+            }
+            else
+            {
+                itemsToCollect.Add(item_name, 1); 
+            }
+        }
+    }
+     
+     //Sets the items in the dictonary to the Shopping list 
+     private void SetText()
+     {
+         textmeshPro.SetText(ToPrettyString());
+         //textmeshPro.SetText(string.Join( "\n", itemsToCollect));
+     }
+    
+     private string ToPrettyString()
+     {
+         string str = "";
+        
+         foreach (var pair in itemsToCollect)
+         {
+             str += string.Format(" {0} {1} \n", pair.Key, pair.Value);
+         }
+        
+         return str;
+     }
+     
+     private void ShowList()
     {
         Vector3 fwd = transform.TransformDirection(Vector3.left);
-        Debug.DrawLine(transform.position, fwd, Color.green);
+        // Debug.DrawLine(transform.position, fwd, Color.green);
         RaycastHit hit;
-        
         
         if (Physics.Raycast(transform.position, fwd, out hit, 10) && hit.collider.CompareTag("Head"))
         { 
@@ -34,20 +97,83 @@ public class ShoppingList : MonoBehaviour
         else 
         {
             isCounting = false;
-            //This happens every frame the user isn't looking at an "option" so this could be optimized. But not a big problem.
-            countdown = timeToShowShoppingList;
-            //textmeshPro.renderer.enabled = false;
-            testCube.SetActive(false);
+            countdown = timeToShowShoppingList; 
+            listObject.SetActive(false);
         }
  
-        if(countdown <= 0f) {
-            //Select option
-           textmeshPro.renderer.enabled = true;
-           testCube.SetActive(true);
-           Debug.Log(countdown);
-           
+        if(countdown <= 0f) 
+        {
+            listObject.SetActive(true);
         }
         if(isCounting) countdown -= Time.deltaTime;
-      
     }
+
+    //need something else cant teleport 
+     private void OnTriggerEnter(Collider other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+       // Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("exit"))
+        {
+            CheckIfItemsCollected();
+        }
+    }
+    private void CheckIfItemsCollected()
+    {
+        Debug.Log(shoppingCart.transform.childCount);
+
+        int forgotten_items = 0;
+        int items_to_much = 0;
+        foreach (var item in itemsToCollect)
+        {
+            int counter_of_items = 0;
+            foreach(Transform child in shoppingCart.transform)
+            {
+                //this could be the problem
+                Debug.Log("foreach loop :" + child.tag);
+                if(child.CompareTag(item.Key))
+                {
+                    counter_of_items++;
+                }
+            }
+            
+           /* for(int i = 0; i < transform.childCount; i++)
+            {
+                Debug.Log("get child loop :" + transform.GetChild(i).tag);
+                /// All your stuff with transform.GetChild(i) here...
+            }*/
+            
+            Debug.Log(counter_of_items + "Counter of items" + " " + item.Key);
+            if(counter_of_items <= item.Value)
+            {
+                forgotten_items += item.Value - counter_of_items;
+            }
+            else
+            {
+                items_to_much += counter_of_items - item.Value;
+            }
+        }
+
+        items_to_much += GetNumberWrongItemsNotOnList();
+        Debug.Log("you forgot " + forgotten_items + "and have " + items_to_much + "to much");
+    }
+
+    private int GetNumberWrongItemsNotOnList()
+    {
+        int wrong_items_count = 0;
+        foreach(Transform child in shoppingCart.transform)
+        {
+            if (!itemsToCollect.ContainsKey(child.tag))
+            {
+                wrong_items_count++;
+            }
+        }
+        return wrong_items_count;
+    }
+    void Update()
+    {
+        ShowList();
+        
+    }
+    
 }
---
+++ unity/Assets/Scripts/TeleportingCart.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportingCart : MonoBehaviour
+{
+    
+    public Transform player;
+     
+    public float distanceTillTeleport = 1f;
+    
+
+    private GameObject findClosestShelf()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("PositionShoppingCart");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = player.transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+    
+    private void teleportShopingCart()
+    {
+        //Debug.Log("TeleportShoppingCart");
+        GameObject closestShelf = findClosestShelf();
+        transform.position = closestShelf.transform.position;
+        transform.rotation = closestShelf.transform.rotation;
+    }
+    void Update()
+    {
+        if (Vector3.Distance(player.position, transform.position) > distanceTillTeleport)
+        {
+            teleportShopingCart();
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.CompareTag("green cube")
+           || other.transform.CompareTag("brown cube")
+        )
+        {
+            other.transform.parent = transform;
+     //       other.transform.tag = "green cube";
+        }
+    }
+}
