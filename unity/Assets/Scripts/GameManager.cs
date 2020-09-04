18b64d98d2fa9f2fe1418cbfd4d29a538d315656
Stefan Kocher <stefan.kocher@student.tugraz.at>
2020-09-04 18:03:39 +0200

    resizing room. adding service bell as button to finish the game.

---
+++ unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+// Game States
+// for later when changing scences
+//public enum GameState { INTRO, MAIN_MENU }
+
+//public delegate void OnStateChangeHandler();
+
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static GameManager instance = null;
+
+    public GameObject shoppingCart;
+    public TextMeshPro exitText;
+
+    protected GameManager()
+    {
+    }
+
+
+
+    // public event OnStateChangeHandler OnStateChange;
+    // public  GameState gameState { get; private set; }
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            throw new Exception("You have two game managers in your scene. You can only have one game manager");
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        ParametersForGame.InitParametersForGame(6);
+    }
+
+    private void Start()
+    {
+        //startGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void startGame()
+    {
+        ShoppingList.instance.SetText(ParametersForGame.Instance().GetItemsToCollect());
+    }
+
+public void endRound()
+    {
+        List<string> itemsInShoppingCart = shoppingCart.GetComponent<ShoppingCart>().GetTagsChildren();
+
+        Score score = CalculateScore.CalcScore(itemsInShoppingCart, ParametersForGame.Instance().GetItemsToCollect());
+        Debug.Log("Round has ended");
+        
+        exitText.SetText("You forgot " + score.GetForgottenItems() + "and have " + score.GetWrongItems() + "to much.");
+        Debug.Log("you forgot " + score.GetForgottenItems() + "and have " + score.GetWrongItems() + "to much");
+        //calculate points
+    }
+    
+    
+    
+}
--- unity/Assets/Scripts/ShoppingList.cs
+++ unity/Assets/Scripts/ShoppingList.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -6,85 +7,109 @@ using TMPro;
 public class ShoppingList : MonoBehaviour
 {
     //outsource all except the List things
-    
+
+    public static ShoppingList instance;
+
     // For showing the list------------------------------
     public GameObject listObject;
     private bool isCounting;
     public float timeToShowShoppingList = 3f;
     private float countdown;
- 
+    public GameObject button;
+
     //For filling the List-------------------------------
     public TextMeshPro textmeshPro;
+
     //private List<string> allItems;
     //this later should go over the server depending on the level of the patient
     //private int amountOfItems = 6;
     //private Dictionary<string, int> itemsToCollect;
     //the shoppingcart to check if all items have been collected
     private GameObject shoppingCart;
-    
+
+
     void Start()
     {
-        listObject.SetActive(false);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            throw new Exception("You have two ShoppingListsScripts in your scene. You can only have one");
+        }
+
+        listObject.SetActive(true);
+        button.SetActive(true);
         isCounting = false;
-        countdown = 3f; 
+        countdown = 3f;
         Debug.Log(gameObject.name);
-        SetText(ParametersForGame.Instance().GetItemsToCollect());
+
         //------------------------------------------
-       /* allItems = new List<string>();
-        allItems.Add("brown cube");
-        allItems.Add("green cube");
-        */
-       // itemsToCollect = new Dictionary<string, int>();
+        /* allItems = new List<string>();
+         allItems.Add("brown cube");
+         allItems.Add("green cube");
+         */
+        // itemsToCollect = new Dictionary<string, int>();
         //RandomItemsToCollect();
         //shoppingCart = GameObject.Find("Shoppingcart");
     }
 
-    
+
     //Sets the items in the dictonary to the Shopping list 
-     private void SetText(Dictionary<string, int> itemsToCollect)
-     {
-         string listText = ToPrettyString(itemsToCollect);
-         textmeshPro.SetText(listText);
-         //textmeshPro.SetText(string.Join( "\n", itemsToCollect));
-     }
-    
-     private string ToPrettyString(Dictionary<string, int> itemsToCollect)
-     {
-         string str = "";
-        
-         foreach (var pair in itemsToCollect)
-         {
-             str += string.Format(" {0} {1} \n", pair.Key, pair.Value);
-         }
-        
-         return str;
-     }
-     
-     private void ShowList()
+    public void SetText(Dictionary<string, int> itemsToCollect)
+    {
+        string listText = ToPrettyString(itemsToCollect);
+        textmeshPro.SetText(listText);
+        //textmeshPro.SetText(string.Join( "\n", itemsToCollect));
+    }
+
+    private string ToPrettyString(Dictionary<string, int> itemsToCollect)
+    {
+        string str = "";
+
+        foreach (var pair in itemsToCollect)
+        {
+            str += string.Format(" {0} {1} \n", pair.Key, pair.Value);
+        }
+
+        return str;
+    }
+
+    private void ShowList()
     {
         Vector3 fwd = transform.TransformDirection(Vector3.left);
         // Debug.DrawLine(transform.position, fwd, Color.green);
         RaycastHit hit;
-        
+
         if (Physics.Raycast(transform.position, fwd, out hit, 10) && hit.collider.CompareTag("Head"))
-        { 
+        {
             isCounting = true;
         }
-        else 
+        else
         {
             isCounting = false;
-            countdown = timeToShowShoppingList; 
+            countdown = timeToShowShoppingList;
             listObject.SetActive(false);
         }
- 
-        if(countdown <= 0f) 
+
+        if (countdown <= 0f)
         {
             listObject.SetActive(true);
         }
-        if(isCounting) countdown -= Time.deltaTime;
+
+        if (isCounting) countdown -= Time.deltaTime;
     }
 
-    //need something else cant teleport 
+    public void startRoundHandler()
+    {
+        Debug.Log("button pushed");
+        GameManager.instance.startGame();
+        button.SetActive(false);
+        listObject.SetActive(false);
+    }
+
+//need something else cant teleport 
     /*
      private void OnTriggerEnter(Collider other)
     {
@@ -98,8 +123,10 @@ public class ShoppingList : MonoBehaviour
   */
     void Update()
     {
-        ShowList();
-        
+        if(!button.activeSelf)
+        {
+            ShowList();
+        }
     }
     
 }
