2c3296c45de6a41bad8ccd5bf3d1509c65095db3
kochstef <kocherstef@gmail.com>
2021-03-17 23:02:48 +0100

    fixed bug with referencing script (but all used scripts in the same folder)

---
+++ unity/Assets/Scripts/HandItemSetup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEditor;
+
+public class HandItemSetup : HandGrabbingBehavior
+{
+    private float pinchThreshhold = 0.5f;
+    
+
+
+
+    private GameObject _grabbableItem;
+
+
+    private string handPose = "";
+
+    private List<OVRBone> _bones;
+
+    private Transform _transform;
+
+
+
+    // public List<OVRBone> _bones;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  base.Start();
+       // m_hand = GetComponent<OVRHand>();
+       // _ovrSkeleton = GetComponent<OVRSkeleton>();
+        //_itemsToSetUp = Resources.LoadAll("pls").Cast<GameObject>().ToArray();
+        //_itemsToSetUp = Resources.LoadAll<GameObject>("pls");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //base.Update();
+       
+    }
+
+}
